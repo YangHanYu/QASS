13d1b19884e1427f2947e8c64d79ff617eb21f0a
HanYu <yangguiguo@163.com>
2018-01-09 19:35:29 +0800

    添加druid

--- src/main/java/com/nsiit/qass/Application.java
+++ src/main/java/com/nsiit/qass/Application.java
@@ -1,12 +1,16 @@
 package com.nsiit.qass;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * @author HanYu
  */
 @SpringBootApplication
+@ServletComponentScan
+@MapperScan("com.nsiit.qass.**.mapper")
 public class Application {
 
     public static void main(String args[]){
